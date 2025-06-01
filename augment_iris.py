b89ae47d94cf7cf1297d341c7e83f4d82bf49ff0
ganeshbmc <ganeshbmc@gmail.com>
2025-06-01 02:51:33 +0000

    augment_iris.py file modified to increase rows by 90 instead of 60

--- augment_iris.py
+++ augment_iris.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.read_csv('data/iris.csv')
 
 # Sample 20 additional rows for each species (with replacement)
-additional_rows = df.groupby('species').apply(lambda x: x.sample(20, replace=True)).reset_index(drop=True)
+additional_rows = df.groupby('species').apply(lambda x: x.sample(30, replace=True)).reset_index(drop=True)
 
 # Concatenate the original and new rows
 augmented_df = pd.concat([df, additional_rows])
@@ -13,4 +13,4 @@ augmented_df = pd.concat([df, additional_rows])
 # Save the new dataset
 augmented_df.to_csv('data/iris.csv', index=False)
 
-print('iris.csv dataset augmented with 60 more rows. Shape {augmented_df.shape}')
+print('iris.csv dataset augmented with 90 more rows. Shape {augmented_df.shape}')
