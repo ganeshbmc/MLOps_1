55f6370751974291a6f7f666821e81e6da0c6caf
ganeshbmc <ganeshbmc@gmail.com>
2025-06-28 18:39:49 +0000

    week3 commit before merging into main

---
+++ .ipynb_checkpoints/augment_iris-checkpoint.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+
+# Load the dataset
+df = pd.read_csv('data/iris.csv')
+
+# Sample 30 additional rows for each species (with replacement)
+additional_rows = df.groupby('species').apply(lambda x: x.sample(20, replace=True)).reset_index(drop=True)
+
+# Concatenate the original and new rows
+augmented_df = pd.concat([df, additional_rows])
+
+# Save the new dataset
+augmented_df.to_csv('data/iris.csv', index=False)
+
+print(f'iris.csv dataset augmented with 60 more rows. Shape {augmented_df.shape}')
