5b41be766f0fa490bac0e656f80ed2151eb93385
ganeshbmc <ganeshbmc@gmail.com>
2025-06-28 22:48:39 +0000

    Still resolving path issues to test file

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -1,14 +1,10 @@
 import os
 import pandas as pd
 
-# Always resolve relative to the repo root
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_PATH = os.path.join(ROOT_DIR, "data", "iris_entity.csv")
-
 def test_data_shape():
-    df = pd.read_csv(DATA_PATH)
+    df = pd.read_csv("../data/iris_entity.csv")
     assert df.shape[0] == 300   # Testing if this file has 300 rows
 
 def test_no_missing_values():
-    df = pd.read_csv(DATA_PATH)
+    df = pd.read_csv("../data/iris_entity.csv")
     assert df.isnull().sum().sum() == 0
--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -7,10 +7,8 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def test_model_accuracy():
     # Load model, label encoder
-    MODEL_PATH = os.path.join(ROOT_DIR, "artifacts", "feast_iris_model.joblib")
-    LABEL_ENC_PATH = os.path.join(ROOT_DIR, "artifacts", "feast_iris_label_encoder.joblib")
-    model = joblib.load(MODEL_PATH)
-    label_encoder = joblib.load(LABEL_ENC_PATH)
+    model = joblib.load("../artifacts/feast_iris_model.joblib")
+    label_encoder = joblib.load("../artifacts/feast_iris_label_encoder.joblib")
 
     entity_df = pd.read_csv("data/iris_entity.csv")
     entity_df["event_timestamp"] = pd.to_datetime(entity_df["event_timestamp"], errors='coerce')
@@ -18,8 +16,7 @@ def test_model_accuracy():
     true_labels = entity_df_2025[["flower_id", "species"]].drop_duplicates()
 
     # Load rows for predicting species using model
-    ONLINE_FEATURES_PATH = os.path.join(ROOT_DIR, "data", "online_featurs_iris.csv")
-    online_features = pd.read_csv(ONLINE_FEATURES_PATH)
+    online_features = pd.read_csv("../data/online_features_iris.csv")
     
     # Predict
     X_online = online_features[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
