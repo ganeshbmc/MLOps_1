4b7f2dd6ad09816b7737e6cf653b91ab21d7517e
ganeshbmc <ganeshbmc@gmail.com>
2025-07-25 15:21:27 +0000

    Modified test files to point to correct file paths

--- tests/.ipynb_checkpoints/test_data_validation-checkpoint.py
+++ tests/.ipynb_checkpoints/test_data_validation-checkpoint.py
@@ -2,12 +2,12 @@ import os
 import pandas as pd
 
 def test_data_shape():
-    df = pd.read_csv("data/iris_entity.csv")
+    df = pd.read_csv("../data/iris_entity.csv")
     print(f"\nLoaded data with shape: {df.shape}")
     assert df.shape[0] == 300   # Testing if this file has 300 rows
 
 def test_no_missing_values():
-    df = pd.read_csv("data/iris_entity.csv")
+    df = pd.read_csv("../data/iris_entity.csv")
     missing = df.isnull().sum()
     total_missing = missing.sum()
     print(f"\nMissing values in data file: {missing}")
